4a5c6cb388d0e714eee23400473aa58c261cea67
Youngtae Kim <youngtae.kim@snowflake.com>
2023-02-10 15:04:22 +0900

    test

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -13,6 +13,7 @@ import sys
 from scipy.constants import convert_temperature
 
 def main(temp_f: float) -> float:
+    #return (float(temp_f) - 32) * (5/9)
     return convert_temperature(float(temp_f), 'F', 'C')
 
 
